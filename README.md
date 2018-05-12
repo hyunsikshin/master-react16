8a5e9b7dd81042e4ca228f1bdcbc344bce2c8e45
hyunsik <hyunsik.dev@gmail.com>
2018-05-12 20:07:36 +0900

    Error Boundaries
    state로 error 유무를 받는 후 componentDidCatch로 에러가 발생했을 때의 상황에 맞게 처리 할 수 있다.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Nomad Academy course to master all the new features of React 16
 
 * Return Types Strings and Fragments
 * Portals
+* Error Boundaries
--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ['hyunsik', 'lynn', 'nicolas', 'biggie', '2pac'],
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined,
+      });
+    }, 2000);
+  };
+
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById('trytouch'));
   }
 }
-
-const Message = () => 'Just touched this!';
 
 class RetrunTypes extends Component {
   render() {
@@ -15,12 +31,29 @@ class RetrunTypes extends Component {
   }
 }
 
+const Message = () => 'Just touched this!';
+
+const ErrorFallBack = () => 'Sorry something went wrong';
+
 class App extends Component {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({
+      hasError: true,
+    });
+  };
+
   render() {
+    const { hasError } = this.setState;
     return (
       <Fragment>
         <RetrunTypes />
         <Portals />
+        {hasError ? <ErrorFallBack /> : <ErrorMaker />}
       </Fragment>
     );
   }
