e19c570bc26b9a664f8df17a013e5c8809db6974
hyunsik <hyunsik.dev@gmail.com>
2018-05-12 19:05:59 +0900

    Function Portal

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # React16
 
 Nomad Academy course to master all the new features of React 16
+
+* Return Types Strings and Fragments
+* Portals
--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { createPortal } from 'react-dom';
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById('trytouch'));
+  }
+}
+
+const Message = () => 'Just touched this!';
+
+class RetrunTypes extends Component {
+  render() {
+    return 'YO SUP?';
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Fragment>
+        <RetrunTypes />
+        <Portals />
+      </Fragment>
+    );
   }
 }
 
