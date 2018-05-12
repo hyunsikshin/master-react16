65eb9a6ded0644058fa4dbeda8607a32231533e1
hyunsik <hyunsik.dev@gmail.com>
2018-05-12 22:09:49 +0900

    Error Boundaries with Higher Order Components
    hasError를 삼항연산자로 구분해서 오류가 났을 경우를 핸들링하는것보다
    HOC를 이용해 component들을 wrapping해서 오류났을 경우 구문을 처리해준다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
+//Higher Order Component -> HOC
+const BoundaryHOC = ProtectedComponent =>
+  class Boundary extends Component {
+    state = {
+      hasError: false,
+    };
+
+    componentDidCatch = () => {
+      this.setState({
+        hasError: true,
+      });
+    };
+
+    render() {
+      const { hasError } = this.state;
+      if (hasError) {
+        return <ErrorFallBack />;
+      } else {
+        return <ProtectedComponent />;
+      }
+    }
+  };
+
 class ErrorMaker extends Component {
   state = {
     friends: ['hyunsik', 'lynn', 'nicolas', 'biggie', '2pac'],
@@ -19,11 +42,15 @@ class ErrorMaker extends Component {
   }
 }
 
+const PErrorMaker = BoundaryHOC(ErrorMaker);
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById('trytouch'));
   }
 }
+
+const PPortals = BoundaryHOC(Portals);
 
 class RetrunTypes extends Component {
   render() {
@@ -36,27 +63,15 @@ const Message = () => 'Just touched this!';
 const ErrorFallBack = () => 'Sorry something went wrong';
 
 class App extends Component {
-  state = {
-    hasError: false,
-  };
-
-  componentDidCatch = (error, info) => {
-    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
-    this.setState({
-      hasError: true,
-    });
-  };
-
   render() {
-    const { hasError } = this.setState;
     return (
       <Fragment>
         <RetrunTypes />
-        <Portals />
-        {hasError ? <ErrorFallBack /> : <ErrorMaker />}
+        <PPortals />
+        <PErrorMaker />
       </Fragment>
     );
   }
 }
 
-export default App;
+export default BoundaryHOC(App);
